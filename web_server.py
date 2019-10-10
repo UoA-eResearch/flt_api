291bf457e50b28244256f705c32aa5a1faec7cc0
Nick <nick.young@auckland.ac.nz>
2019-10-11 10:31:26 +1300

    add requirements.txt and test_data support csv file upload

--- web_server.py
+++ web_server.py
@@ -3,9 +3,12 @@ import rasterio
 import glob
 import sys
 from pyproj import Proj, transform
-from bottle import Bottle, run, request
+from bottle import *
+BaseRequest.MEMFILE_MAX = 1e8
 import time
 import json
+import os
+import csv
 
 s = time.time()
 files = glob.glob("data/*.flt")
@@ -17,15 +20,26 @@ def contains(bounds, point):
     return point[0] > bounds.left and point[0] < bounds.right and point[1] > bounds.bottom and point[1] < bounds.top
 
 app = Bottle()
-@app.route('/')
+@app.route('/', method=['GET', 'POST'])
 def main():
-    points = json.loads(request.params.get("points"))
-    print(points)
+    try:
+        points = json.loads(request.params.get("points"))
+    except:
+        decoded = request.files.get("points").file.read().decode("utf-8").split('\n')
+        reader = csv.reader(decoded, delimiter=",")
+        points = []
+        for row in reader:
+            try:
+                y = float(row[0])
+                x = float(row[1])
+                points.append((y, x))
+            except:
+                continue
+    print(f"Got {len(points)} points")
     proj = request.params.get("proj", "EPSG:4326")
     inProj = Proj(init=proj)
     outProj = Proj(init='EPSG:2193')
     points = [transform(inProj, outProj, x, y) for y,x in points]
-    print(points)
 
     results = [0] * len(points)
     for raster in rasters:
@@ -35,4 +49,7 @@ def main():
                 results[i] = float(list(vals)[0][0])
     return {"results": results}
 
-run(app, host='localhost', port=8080)
+port = int(os.environ.get('PORT', 8080))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=port, debug=True, server='gunicorn', workers=4)
